c6e8bc72d277cd44d331e10a3b462962e347ab91
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-08-17 11:59:52 +0530

    Main method variations: Method Signature Variations

--- module-01-java-basics/src/revisit/submodule01/mainvariants/MainVariant03.java
+++ module-01-java-basics/src/revisit/submodule01/mainvariants/MainVariant03.java
@@ -2,6 +2,6 @@ package revisit.submodule01.mainvariants;
 
 public class MainVariant03 {
     public static void main(String args[]) {
-        System.out.println("The array can be declared with square brackets after the variable name.");
+        System.out.println("The array can be declared with square brackets after the variable name");
     }
 }
---
+++ module-01-java-basics/src/revisit/submodule01/mainvariants/MainVariant04.java
@@ -0,0 +1,7 @@
+package revisit.submodule01.mainvariants;
+
+public class MainVariant04 {
+    public static void main(String... args) {
+        System.out.println("The ellipsis ‘(...)’ syntax is used for varargs, which is also valid.");
+    }
+}
