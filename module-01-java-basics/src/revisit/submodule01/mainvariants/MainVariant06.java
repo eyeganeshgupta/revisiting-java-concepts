b18cf143424038baf68f7d08d71a61e16211bb22
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-08-17 12:15:44 +0530

    main method with final keyword

---
+++ module-01-java-basics/src/revisit/submodule01/mainvariants/MainVariant06.java
@@ -0,0 +1,7 @@
+package revisit.submodule01.mainvariants;
+
+public class MainVariant06 {
+    final public static void main(String[] args) {
+        System.out.println("The ‘final’ keyword in Java is used to indicate that something cannot be changed or overridden. When applied to the ‘main’ method, it means that the method cannot be overridden by any subclass. However, since the ‘main’ method is ‘static’, it belongs to the class rather than an instance, and it's not inherited in the usual way. As a result, the use of ‘final’ here is more of a formality and has no significant effect on the functionality of the ‘main’ method.");
+    }
+}
