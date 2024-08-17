f7b1911eb58ed7150ee75365e879e716d0a7bc34
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-08-17 12:02:57 +0530

    Main method variations: Without Access Modifier

---
+++ module-01-java-basics/src/revisit/submodule01/mainvariants/MainVariant05.java
@@ -0,0 +1,7 @@
+package revisit.submodule01.mainvariants;
+
+public class MainVariant05 {
+    static void main(String[] args) {
+        System.out.println("The ‘public’ access modifier can be omitted, but the method will not be accessible outside its package, hence not ideal for most applications.");
+    }
+}
