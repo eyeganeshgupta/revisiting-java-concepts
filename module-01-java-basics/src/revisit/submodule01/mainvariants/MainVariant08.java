3d101132dfdda0355ed51a906d5499f30c73dd1d
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-08-18 08:51:49 +0530

    main() method inheritance

--- module-01-java-basics/src/revisit/submodule01/mainvariants/MainVariant08.java
+++ module-01-java-basics/src/revisit/submodule01/mainvariants/MainVariant08.java
@@ -2,7 +2,8 @@ package revisit.submodule01.mainvariants;
 
 class Parent {
     public static void main(String[] args) {
-        System.out.println("Parent main method called!");
+        System.out.println("\nParent main method called!");
+        System.out.println("\nNOTE: main() method gets inherited and if the child class doesn't has a main() method and we run it then the parent class main() method will be called by JVM.");
     }
 }
 
