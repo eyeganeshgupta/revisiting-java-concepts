b087d0e5b9b59a512280164fdc9cb647c9793aa8
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-08-18 09:15:17 +0530

    main() method overloading

---
+++ module-01-java-basics/src/revisit/submodule01/mainvariants/MainVariant10.java
@@ -0,0 +1,7 @@
+package revisit.submodule01.mainvariants;
+
+public class MainVariant10 extends Parent {
+    public void main(int[] args) {
+        System.out.println("Child main method called!");
+    }
+}
